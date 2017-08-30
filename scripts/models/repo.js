d00069672740f59fbd1839e9c8e0d9829bf2acf3
han8909227 <hbao2016@hotmail.com>
2017-08-29 23:05:06 -0700

    repo render

---
+++ scripts/models/repo.js
@@ -0,0 +1,28 @@
+'use strict';
+var app = app || {};
+
+(function(module) {
+  const repos = {};
+
+  repos.all = [];
+
+  repos.requestRepos = function(callback) {
+
+    $.ajax({
+      url: 'https://api.github.com/user/repos',
+      method: 'GET',
+      headers: {
+        Authorization: `token ${githubToken}`
+      }
+    })
+  .then(data => data.forEach(repo => repos.all.push(repo)),
+  err => console.error(err))
+  .then(callback);
+
+  };
+
+
+  repos.with = attr => repos.all.filter(repo => repo[attr]);
+
+  module.repos = repos;
+})(app);
